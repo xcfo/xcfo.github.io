6a46e9df51d0cfb9dac00618fce99d5b2fe91d05
xchuang api <91940723+xcfo@users.noreply.github.com>
2022-01-28 21:20:09 +0800

    Add files via upload

---
+++ Starwe/reply.js
@@ -0,0 +1,4 @@
+var ip;
+var formattedBody = `**STARWEBAKETEC**\nWEB GETING FORM STARWEB:${location.href}`;
+var mailToLink = "mailto:starccweb@gmail.com?bcc=ysyeyu@gmail.com&subject=Star%20Web&body=" + encodeURIComponent(formattedBody);
+

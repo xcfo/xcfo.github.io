7ae024da5fbf29bff4e90af45445aaa0307f1c09
xchuang api <91940723+xcfo@users.noreply.github.com>
2022-01-28 21:25:59 +0800

    Add files via upload

--- Starwe/reply.js
+++ Starwe/reply.js
@@ -1,4 +1,15 @@
 var ip;
-var formattedBody = `**STARWEBAKETEC**\nWEB GETING FORM STARWEB:${location.href}`;
-var mailToLink = "mailto:starccweb@gmail.com?bcc=ysyeyu@gmail.com&subject=Star%20Web&body=" + encodeURIComponent(formattedBody);
+var time = new Date();
+var time_now = time.getTime();
+var time_now_str = time_now.toString();
+var time_now_str_len = time_now_str.length;
+var time_now_str_len_half = time_now_str_len / 2;
+var time_now_str_len_half_floor = Math.floor(time_now_str_len_half);
+var time_now_str_len_half_ceil = Math.ceil(time_now_str_len_half);
+var time_now_str_len_half_floor_str = time_now_str_len_half_floor.toString();
+var time_now_str_len_half_ceil_str = time_now_str_len_half_ceil.toString();
+var time_now_str_len_half_floor_str_len = time_now_str_len_half_floor_str.length;
+
+var formattedBody = `**STARWEBAKETEC**\nWEB GETING FORM STARWEB:${location.href}&${time_now}${time_now_str_len_half_floor_str}${time_now_str_len_half_ceil_str}/${time_now_str_len_half_floor_str_len}`;
+var mailToLink = "mailto:xchuangc@163.com?bcc=ysyeyu@gmail.com&subject=Star%20Web&body=" + encodeURIComponent(formattedBody);
 
